3f8d2403d380fe374d1846f3d3e5c5a3d301f1a4
Tsintsabadze <ekacincabadze@gmail.com>
2024-01-24 18:28:48 +0100

    commit

---
+++ GOA/Day,3.py/homework.py
@@ -0,0 +1,21 @@
+# მომხმარებელს შემოატანინეთ ასაკი, შემოტანილი ასაკი გაამრავლეთ 7 ზე და გაყეთ  3 ზე და მიღებულ ასაკს დაუმატეთ  4 სა და ცხრის ნამრავლის ჯამი
+
+age = int(input("enter your age "))
+
+age_multiple = age * 7
+
+age_division = age_multiple / 3
+
+age_plus = age_division + (9 * 4)
+
+print(age_plus)
+
+
+
+
+
+
+
+
+
+
